13170abba0412e011657e89c7ba7af9a2556000a
janektichy <janek.tichy@email.cz>
2021-08-10 13:32:37 +0200

    oh yeah

--- scripts.js
+++ scripts.js
@@ -1,17 +1,17 @@
 
-//$('#addtodo').val('empty')
-//("#addtodo".val.length < 1 )
 
 $('#addbtn').on('click', function() {
-    if ($('#addtodo').val().length == 0) {
+    if ($('#addtodo').val() == '') {
         console.log("I just don't know how to solve this problem in a different way:)")
     } else {
-             $('#todolist').append("<li>"+$('#addtodo').val()+"</li>")};
-                $('li').on('click', function () {
-                    $(this).css('textDecoration', 'line-through')
-        });
-    });
+            $('#todolist').append("<li>"+$('#addtodo').val()+"</li>");
+            $('li').last('li').on('click', function () {
+                $(this).css('textDecoration', 'line-through')
+            console.log('ahoj')
+            })           
+    };
+});
 
 $('li').on('click', function () {
-    $(this).css('textDecoration', 'line-through')
+   $(this).addClass('crosed');   
 });
